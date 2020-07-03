1eb64bab6e3ba378f50e88d2a062e78d43db412a
afropapp13 <afropapp@yahoo.gr>
2020-07-03 07:39:32 -0500

    Jul 3 2020: editting the script_DetVars file to loop over the Run1 light samples and the Run3 light & wiremod samples

--- script_DetVar_PreSelection.C
+++ script_DetVar_PreSelection.C
@@ -3,13 +3,37 @@
 	vector<TString> WhichSampleArray;
 
 	// Detector Systematics // Starting from the same GENIE events
+
+	/*
+	// v33
        
 	WhichSampleArray.push_back("Overlay9_Run1_CV");
 	WhichSampleArray.push_back("Overlay9_Run1_X");
 	WhichSampleArray.push_back("Overlay9_Run1_YZ");
 	WhichSampleArray.push_back("Overlay9_Run1_LY");
 	WhichSampleArray.push_back("Overlay9_Run1_LYRayleigh");
-	
+	*/
+
+	//v43
+
+	WhichSampleArray.push_back("Overlay9_Run1_CV");
+        WhichSampleArray.push_back("Overlay9_Run1_LYDown");
+	WhichSampleArray.push_back("Overlay9_Run1_LYRayleigh");
+	WhichSampleArray.push_back("Overlay9_Run1_LYAttenuation");
+
+	WhichSampleArray.push_back("Overlay9_Run3_CV");
+        WhichSampleArray.push_back("Overlay9_Run3_LYDown");
+	WhichSampleArray.push_back("Overlay9_Run3_LYRayleigh");
+	WhichSampleArray.push_back("Overlay9_Run3_LYAttenuation");
+
+	WhichSampleArray.push_back("Overlay9_Run3_WireModX");
+	WhichSampleArray.push_back("Overlay9_Run3_WireModYZ");
+	WhichSampleArray.push_back("Overlay9_Run3_WireModThetaYZ");
+	WhichSampleArray.push_back("Overlay9_Run3_WireModThetaXZ");
+	WhichSampleArray.push_back("Overlay9_Run3_dEdx");
+	WhichSampleArray.push_back("Overlay9_Run3_Recombination2");
+	WhichSampleArray.push_back("Overlay9_Run3_SCE");
+
 	// -------------------------------------------------------
 
 	gROOT->ProcessLine(".L ../../myClasses/Tools.cxx+");
