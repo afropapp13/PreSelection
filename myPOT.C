56edc31ef82493ce1a45235bcc5d81d9cca08c71
afropapp13 <afropapp@yahoo.gr>
2020-09-14 15:52:23 -0500

    v43 Sept 14 2020: storing the POT plot under /uboone/data/users/apapadop/myEvents/mySamples/UBCodeVersion

--- myPOT.C
+++ myPOT.C
@@ -38,7 +38,7 @@ void myPOT::Loop() {
 
   // Output File
 
-  TString FileName = "/uboone/data/users/apapadop/myEvents/"+UBCodeVersion+"/PreSelection_"+fWhichSample+"_"+UBCodeVersion+"_POT.root";
+  TString FileName = "/uboone/data/users/apapadop/myEvents/mySamples/"+UBCodeVersion+"/PreSelection_"+fWhichSample+"_"+UBCodeVersion+"_POT.root";
   TFile* OutputFile = new TFile(FileName,"recreate");
   OutputFile->cd();
   POTCountHist->Write();
