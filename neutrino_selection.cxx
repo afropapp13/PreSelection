179824f6945c0b0e474ff909fb3ccd6c7d951741
afropapp13 <afropapp@yahoo.gr>
2024-02-01 15:22:13 -0600

    Feb 1 2024: adding nuwro run4 & half of the detector variations

--- neutrino_selection.cxx
+++ neutrino_selection.cxx
@@ -536,7 +536,7 @@ void neutrino_selection::Loop() {
 
 	if (string(fLabel).find("Overlay") != std::string::npos) {
 
-		TString PathToPOTFile = "/uboone/data/users/apapadop/PeLEETuples_3D_ECal/PreSelection_"+fLabel+"_"+UBCodeVersion+"_POT.root";		
+		TString PathToPOTFile = "/uboone/data/users/apapadop/PeLEETuples_Atmospherics/PreSelection_"+fLabel+"_"+UBCodeVersion+"_POT.root";		
 
 		TFile* POTFile = TFile::Open(PathToPOTFile,"readonly");
 		TH1D* POTCountHist = (TH1D*)(POTFile->Get("POTCountHist"));
