8603f32ca6ec0485e7aca4c0060e7991bdac694a
afropapp13 <afropapp@yahoo.gr>
2024-02-27 14:12:36 -0600

    Feb 27 2024: migration to new /exp areas

--- merge_files.sh
+++ merge_files.sh
@@ -51,14 +51,14 @@ hadd -f ${path_in}/PreSelection_Overlay9_Combined_LYAttenuation_${version}_POT.r
 hadd -f ${path_in}/PreTruthSelection_Overlay9_Combined_LYAttenuation_${version}.root ${path_in}/PreTruthSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_LYAttenuation_${version}.root
 
 # SCE
-#hadd -f ${path_in}/PreSelection_Overlay9_Combined_SCE_${version}.root ${path_in}/PreSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_SCE_${version}.root
-#hadd -f ${path_in}/PreSelection_Overlay9_Combined_SCE_${version}_POT.root ${path_in}/PreSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_SCE_${version}_POT.root
-#hadd -f ${path_in}/PreTruthSelection_Overlay9_Combined_SCE_${version}.root ${path_in}/PreTruthSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_SCE_${version}.root
+hadd -f ${path_in}/PreSelection_Overlay9_Combined_SCE_${version}.root ${path_in}/PreSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_SCE_${version}.root
+hadd -f ${path_in}/PreSelection_Overlay9_Combined_SCE_${version}_POT.root ${path_in}/PreSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_SCE_${version}_POT.root
+hadd -f ${path_in}/PreTruthSelection_Overlay9_Combined_SCE_${version}.root ${path_in}/PreTruthSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_SCE_${version}.root
 
 # Recombinations2
-#hadd -f ${path_in}/PreSelection_Overlay9_Combined_Recombination2_${version}.root ${path_in}/PreSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_Recombination2_${version}.root
-#hadd -f ${path_in}/PreSelection_Overlay9_Combined_Recombination2_${version}_POT.root ${path_in}/PreSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_Recombination2_${version}_POT.root
-#hadd -f ${path_in}/PreTruthSelection_Overlay9_Combined_Recombination2_${version}.root ${path_in}/PreTruthSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_Recombination2_${version}.root
+hadd -f ${path_in}/PreSelection_Overlay9_Combined_Recombination2_${version}.root ${path_in}/PreSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_Recombination2_${version}.root
+hadd -f ${path_in}/PreSelection_Overlay9_Combined_Recombination2_${version}_POT.root ${path_in}/PreSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_Recombination2_${version}_POT.root
+hadd -f ${path_in}/PreTruthSelection_Overlay9_Combined_Recombination2_${version}.root ${path_in}/PreTruthSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_Recombination2_${version}.root
 
 # X
 hadd -f ${path_in}/PreSelection_Overlay9_Combined_X_${version}.root ${path_in}/PreSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_X_${version}.root
@@ -66,19 +66,19 @@ hadd -f ${path_in}/PreSelection_Overlay9_Combined_X_${version}_POT.root ${path_i
 hadd -f ${path_in}/PreTruthSelection_Overlay9_Combined_X_${version}.root ${path_in}/PreTruthSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_X_${version}.root
 
 # YZ
-#hadd -f ${path_in}/PreSelection_Overlay9_Combined_YZ_${version}.root ${path_in}/PreSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_YZ_${version}.root
-#hadd -f ${path_in}/PreSelection_Overlay9_Combined_YZ_${version}_POT.root ${path_in}/PreSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_YZ_${version}_POT.root
-#hadd -f ${path_in}/PreTruthSelection_Overlay9_Combined_YZ_${version}.root ${path_in}/PreTruthSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_YZ_${version}.root
+hadd -f ${path_in}/PreSelection_Overlay9_Combined_YZ_${version}.root ${path_in}/PreSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_YZ_${version}.root
+hadd -f ${path_in}/PreSelection_Overlay9_Combined_YZ_${version}_POT.root ${path_in}/PreSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_YZ_${version}_POT.root
+hadd -f ${path_in}/PreTruthSelection_Overlay9_Combined_YZ_${version}.root ${path_in}/PreTruthSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_YZ_${version}.root
 
 ## ThetaXZ
-#hadd -f ${path_in}/PreSelection_Overlay9_Combined_ThetaXZ_${version}.root ${path_in}/PreSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_ThetaXZ_${version}.root
-#hadd -f ${path_in}/PreSelection_Overlay9_Combined_ThetaXZ_${version}_POT.root ${path_in}/PreSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_ThetaXZ_${version}_POT.root
-#hadd -f ${path_in}/PreTruthSelection_Overlay9_Combined_ThetaXZ_${version}.root ${path_in}/PreTruthSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_ThetaXZ_${version}.root
+hadd -f ${path_in}/PreSelection_Overlay9_Combined_ThetaXZ_${version}.root ${path_in}/PreSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_ThetaXZ_${version}.root
+hadd -f ${path_in}/PreSelection_Overlay9_Combined_ThetaXZ_${version}_POT.root ${path_in}/PreSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_ThetaXZ_${version}_POT.root
+hadd -f ${path_in}/PreTruthSelection_Overlay9_Combined_ThetaXZ_${version}.root ${path_in}/PreTruthSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_ThetaXZ_${version}.root
 
 # ThetaYZ
-#hadd -f ${path_in}/PreSelection_Overlay9_Combined_ThetaYZ_${version}.root ${path_in}/PreSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_ThetaYZ_${version}.root
-#hadd -f ${path_in}/PreSelection_Overlay9_Combined_ThetaYZ_${version}_POT.root ${path_in}/PreSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_ThetaYZ_${version}_POT.root
-#hadd -f ${path_in}/PreTruthSelection_Overlay9_Combined_ThetaYZ_${version}.root ${path_in}/PreTruthSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_ThetaYZ_${version}.root
+hadd -f ${path_in}/PreSelection_Overlay9_Combined_ThetaYZ_${version}.root ${path_in}/PreSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_ThetaYZ_${version}.root
+hadd -f ${path_in}/PreSelection_Overlay9_Combined_ThetaYZ_${version}_POT.root ${path_in}/PreSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_ThetaYZ_${version}_POT.root
+hadd -f ${path_in}/PreTruthSelection_Overlay9_Combined_ThetaYZ_${version}.root ${path_in}/PreTruthSelection_Overlay9_Run{1,2,3,4b,4c,4d,5}_ThetaYZ_${version}.root
 
 ######################
 
--- run_detvar_preselection.cxx
+++ run_detvar_preselection.cxx
@@ -32,6 +32,7 @@
 	WhichSampleArray.push_back("Overlay9_Run2_SCE"); SamplePath.push_back("/uboone/data/users/apapadop/searchingfornues/high_stat_prodgenie_bnb_nu_overlay_DetVar_Run3_SCE_reco2_reco2.root");
 	WhichSampleArray.push_back("Overlay9_Run2_Recombination2"); SamplePath.push_back("/uboone/data/users/apapadop/searchingfornues/high_stat_prodgenie_bnb_nu_overlay_DetVar_Run3_Recombination_reco2_reco2.root");
 
+
 	// Run3
 
 	WhichSampleArray.push_back("Overlay9_Run3_CV"); SamplePath.push_back("/uboone/data/users/apapadop/searchingfornues/high_stat_prodgenie_bnb_nu_overlay_DetVar_Run3_CV_reco2_reco2.root");
@@ -54,6 +55,11 @@
 	WhichSampleArray.push_back("Overlay9_Run4b_LYRayleigh"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_5_bnb_nu_overlay_detvar_LYRayleigh_reco2_pandora_unified_run4_ana.root");
 	WhichSampleArray.push_back("Overlay9_Run4b_LYAttenuation"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/Run_4_BNB_Nu_Detvar_LYAttenuation_Pandora_Reco2_run4_ana.root");
 	WhichSampleArray.push_back("Overlay9_Run4b_X"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_wiremod_x_pandora_reco2_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4b_YZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_wiremod_yz_pandora_reco2_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4b_ThetaYZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_Pandora_WireMod_Theta_YZ_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4b_ThetaXZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_Pandora_WireMod_Theta_XZ_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4b_SCE"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_SCE_pandora_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4b_Recombination2"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_Recomb2_pandora_reco2_ana.root");
 	
 	// Run4c
 	
@@ -63,6 +69,11 @@
 	WhichSampleArray.push_back("Overlay9_Run4c_LYRayleigh"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_5_bnb_nu_overlay_detvar_LYRayleigh_reco2_pandora_unified_run4_ana.root");
 	WhichSampleArray.push_back("Overlay9_Run4c_LYAttenuation"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/Run_4_BNB_Nu_Detvar_LYAttenuation_Pandora_Reco2_run4_ana.root");
 	WhichSampleArray.push_back("Overlay9_Run4c_X"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_wiremod_x_pandora_reco2_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4c_YZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_wiremod_yz_pandora_reco2_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4c_ThetaYZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_Pandora_WireMod_Theta_YZ_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4c_ThetaXZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_Pandora_WireMod_Theta_XZ_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4c_SCE"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_SCE_pandora_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4c_Recombination2"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_Recomb2_pandora_reco2_ana.root");
 	
 	// Run4d
 	
@@ -72,6 +83,11 @@
 	WhichSampleArray.push_back("Overlay9_Run4d_LYRayleigh"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_5_bnb_nu_overlay_detvar_LYRayleigh_reco2_pandora_unified_run4_ana.root");
 	WhichSampleArray.push_back("Overlay9_Run4d_LYAttenuation"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/Run_4_BNB_Nu_Detvar_LYAttenuation_Pandora_Reco2_run4_ana.root");
 	WhichSampleArray.push_back("Overlay9_Run4d_X"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_wiremod_x_pandora_reco2_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4d_YZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_wiremod_yz_pandora_reco2_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4d_ThetaYZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_Pandora_WireMod_Theta_YZ_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4d_ThetaXZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_Pandora_WireMod_Theta_XZ_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4d_SCE"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_SCE_pandora_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4d_Recombination2"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_Recomb2_pandora_reco2_ana.root");
 	
 	// Run5
 	
@@ -81,10 +97,12 @@
 	WhichSampleArray.push_back("Overlay9_Run5_LYRayleigh"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run5_detvar/run4_5_bnb_nu_overlay_detvar_LYRayleigh_reco2_pandora_unified_run5_ana.root");
 	WhichSampleArray.push_back("Overlay9_Run5_LYAttenuation"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run5_detvar/Run_5_BNB_Nu_Detvar_LYAttenuation_Pandora_Reco2_run5_ana.root");
 	WhichSampleArray.push_back("Overlay9_Run5_X"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run5_detvar/run5_bnb_nu_detvar_wire_mod_x_Pandora_reco2_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run5_YZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run5_detvar/run5_bnb_nu_detvar_wire_mod_yz_Pandora_reco2_reco2_ana.root");
 	WhichSampleArray.push_back("Overlay9_Run5_ThetaXZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run5_detvar/run5_bnb_nu_detvar_Pandora_WireMod_Theta_XZ_reco2_reco2_ana.root");
-	WhichSampleArray.push_back("Overlay9_Run5_ThetaYZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run5_detvar/run5_bnb_nu_detvar_Pandora_WireMod_Theta_YZ_reco2_reco2_ana.root");
+        WhichSampleArray.push_back("Overlay9_Run5_ThetaYZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run5_detvar/run5_bnb_nu_detvar_Pandora_WireMod_Theta_YZ_reco2_reco2_ana.root");	
+	WhichSampleArray.push_back("Overlay9_Run5_SCE"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run5_detvar/run5_bnb_nu_detvar_SCE_pandora_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run5_Recombination2"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run5_detvar/run5_bnb_nu_detvar_Recomb2_pandora_reco2_ana.root");
 	
-
 	// ----------------------------------------------------------------------------------
 
 	gROOT->ProcessLine(".L ../myClasses/Tools.cxx+");
