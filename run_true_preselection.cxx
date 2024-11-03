8ea590871e494e7dafd3fc72d9e7d3de4a276607
afropapp13 <afropapp@yahoo.gr>
2024-11-03 16:48:57 -0600

    Nov 3 2024: adding 1A & 1B samples to preselection chain

--- run_true_preselection.cxx
+++ run_true_preselection.cxx
@@ -10,6 +10,8 @@
 	// MC CV
 
 	WhichSampleArray.push_back("Overlay9_Run1"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run1/overlay_peleeTuple_uboone_v08_00_00_70_run1_nu.root");
+	WhichSampleArray.push_back("Overlay9_Run1A_open_trigger"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run1/overlay_peleeTuple_uboone_v08_00_00_70_run1_nu.root");
+	WhichSampleArray.push_back("Overlay9_Run1B_open_trigger"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run1/overlay_peleeTuple_uboone_v08_00_00_70_run1_nu.root");
 	WhichSampleArray.push_back("Overlay9_Run2"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run2/overlay_peleeTuple_uboone_v08_00_00_70_run2_nu.root");
 	WhichSampleArray.push_back("Overlay9_Run3"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3/overlay_peleeTuple_uboone_v08_00_00_70_run3_nu.root");
 	WhichSampleArray.push_back("Overlay9_Run4a"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4a/run4a_bnb_nu_overlay_unified_pandora_reco2_run4a_ana.root");
@@ -21,6 +23,8 @@
 	// Dirt
 
 	WhichSampleArray.push_back("OverlayDirt9_Run1"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run1/overlay_peleeTuple_uboone_v08_00_00_70_run1_dirt.root");
+	WhichSampleArray.push_back("OverlayDirt9_Run1A_open_trigger"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run1/overlay_peleeTuple_uboone_v08_00_00_70_run1_dirt.root");
+	WhichSampleArray.push_back("OverlayDirt9_Run1B_open_trigger"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run1/overlay_peleeTuple_uboone_v08_00_00_70_run1_dirt.root");
 	WhichSampleArray.push_back("OverlayDirt9_Run2"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run2/overlay_peleeTuple_uboone_v08_00_00_70_run2a_dirt.root");
 	WhichSampleArray.push_back("OverlayDirt9_Run3"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3/overlay_peleeTuple_uboone_v08_00_00_70_run3_dirt.root");
 	WhichSampleArray.push_back("OverlayDirt9_Run4a"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4a/Run4a_BNB_dirt_overlay_Pandora_Unified_Reco2_fixed_run4a_ana.root");
@@ -35,6 +39,8 @@
 	// NuWro
 
 	WhichSampleArray.push_back("Overlay9NuWro_Run1"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run1/high_stat_prodgenie_bnb_nu_overlay_DetVar_Run1_NuWro_reco2_reco2.root");
+	WhichSampleArray.push_back("Overlay9NuWro_Run1A_open_trigger"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run1/high_stat_prodgenie_bnb_nu_overlay_DetVar_Run1_NuWro_reco2_reco2.root");
+	WhichSampleArray.push_back("Overlay9NuWro_Run1B_open_trigger"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run1/high_stat_prodgenie_bnb_nu_overlay_DetVar_Run1_NuWro_reco2_reco2.root");
 	WhichSampleArray.push_back("Overlay9NuWro_Run2"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run2/high_stat_prodgenie_bnb_nu_overlay_DetVar_Run2_NuWro_reco2_reco2.root");
 	WhichSampleArray.push_back("Overlay9NuWro_Run3"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3/high_stat_prodgenie_bnb_nu_overlay_DetVar_Run3_NuWro_reco2_reco2.root");
 	WhichSampleArray.push_back("Overlay9NuWro_Run4a"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4a/high_stat_prodgenie_bnb_nu_nuwro_overlay_run4a_pelee.root");
@@ -62,6 +68,39 @@ WhichSampleArray.push_back("Overlay9_Run1_X"); SamplePath.push_back("/pnfs/uboon
 WhichSampleArray.push_back("Overlay9_Run1_ThetaXZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_WireModThetaXZ_reprocess_ana.root");
 	WhichSampleArray.push_back("Overlay9_Run1_ThetaYZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_WireModThetaYZ_reprocess_ana.root");
 	
+	// Run1A_open_trigger
+	// Using the Run3 detector variations with Run1A_open_trigger scaling
+
+	WhichSampleArray.push_back("Overlay9_Run1A_open_trigger_CV"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_CV_reprocess_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run1A_open_trigger_CVextra"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_CVExtra_reprocess_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run1A_open_trigger_LYDown"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_LYDown_reprocess_ana.root");	
+	WhichSampleArray.push_back("Overlay9_Run1A_open_trigger_LYAttenuation"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_LYRayleigh_reprocess_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run1A_open_trigger_LYRayleigh"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_LYAttenuation_reprocess_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run1A_open_trigger_SCE"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_SCE_reprocess_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run1A_open_trigger_Recombination2"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_Recomb2_reprocess_ana.root");
+
+	WhichSampleArray.push_back("Overlay9_Run1A_open_trigger_X"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_WireModX_reprocess_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run1A_open_trigger_YZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_WireModYZ_reprocess_ana.root");
+WhichSampleArray.push_back("Overlay9_Run1A_open_trigger_ThetaXZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_WireModThetaXZ_reprocess_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run1A_open_trigger_ThetaYZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_WireModThetaYZ_reprocess_ana.root");
+	
+	// Run1B_open_trigger
+	// Using the Run3 detector variations with Run1B_open_trigger scaling
+
+	WhichSampleArray.push_back("Overlay9_Run1B_open_trigger_CV"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_CV_reprocess_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run1B_open_trigger_CVextra"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_CVExtra_reprocess_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run1B_open_trigger_LYDown"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_LYDown_reprocess_ana.root");	
+	WhichSampleArray.push_back("Overlay9_Run1B_open_trigger_LYAttenuation"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_LYRayleigh_reprocess_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run1B_open_trigger_LYRayleigh"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_LYAttenuation_reprocess_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run1B_open_trigger_SCE"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_SCE_reprocess_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run1B_open_trigger_Recombination2"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_Recomb2_reprocess_ana.root");
+
+	WhichSampleArray.push_back("Overlay9_Run1B_open_trigger_X"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_WireModX_reprocess_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run1B_open_trigger_YZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_WireModYZ_reprocess_ana.root");
+WhichSampleArray.push_back("Overlay9_Run1B_open_trigger_ThetaXZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_WireModThetaXZ_reprocess_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run1B_open_trigger_ThetaYZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_WireModThetaYZ_reprocess_ana.root");
+	
+
 	// Run2
 	// Using the Run3 files with Run2 scaling
 
@@ -93,6 +132,20 @@ WhichSampleArray.push_back("Overlay9_Run3_X"); SamplePath.push_back("/pnfs/uboon
 	WhichSampleArray.push_back("Overlay9_Run3_YZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_WireModYZ_reprocess_ana.root");
 WhichSampleArray.push_back("Overlay9_Run3_ThetaXZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_WireModThetaXZ_reprocess_ana.root");
 	WhichSampleArray.push_back("Overlay9_Run3_ThetaYZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run3_detvar/overlay_pelee_ntuples_bnb_nu_detvar_run3_WireModThetaYZ_reprocess_ana.root");
+	
+	// Run4a
+	
+	WhichSampleArray.push_back("Overlay9_Run4a_CV"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/Detvar_BNB_nu_pandora_reco2_CV_run4_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4a_CVextra"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/Detvar_BNB_nu_pandora_reco2_CV_run4_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4a_LYDown"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_5_bnb_nu_overlay_detvar_LYDown_reco2_pandora_unified_run4_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4a_LYAttenuation"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/Run_4_BNB_Nu_Detvar_LYAttenuation_Pandora_Reco2_run4_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4a_LYRayleigh"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_5_bnb_nu_overlay_detvar_LYRayleigh_reco2_pandora_unified_run4_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4a_SCE"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_SCE_pandora_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4a_Recombination2"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_Recomb2_pandora_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4a_X"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_wiremod_x_pandora_reco2_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4a_YZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_wiremod_yz_pandora_reco2_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4a_ThetaXZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_Pandora_WireMod_Theta_XZ_reco2_ana.root");
+	WhichSampleArray.push_back("Overlay9_Run4a_ThetaYZ"); SamplePath.push_back("/pnfs/uboone/persistent/users/cthorpe/PELEE_2023/run4_detvar/run4_bnb_nu_detvar_Pandora_WireMod_Theta_YZ_reco2_ana.root");
 	
 	// Run4b
 	
@@ -168,4 +221,5 @@ WhichSampleArray.push_back("Overlay9_Run3_ThetaXZ"); SamplePath.push_back("/pnfs
 	}
 
 	gROOT->ProcessLine(".q");
+
 };
